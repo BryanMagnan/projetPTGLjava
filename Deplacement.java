6a82e804b3be7de6bd5132bad80f99ad53f4143b
LegrandHugo <36848957+LegrandHugo@users.noreply.github.com>
2018-02-26 13:58:08 +0100

    Create Deplacement.java

---
+++ Deplacement.java
@@ -0,0 +1,18 @@
+
+public class Deplacement 
+{
+	private double temps;
+	private double destX;
+	private double destY;
+	private double destZ;
+	private double speed;
+	
+	public Deplacement(double temps, double destX, double destY, double destZ, double speed) 
+	{
+		this.temps = temps;
+		this.destX = destX;
+		this.destY = destY;
+		this.destZ = destZ;
+		this.speed = speed;
+	}
+}
