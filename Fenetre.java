5cea497997574379c7dc7433596f1a81f51b40c5
LegrandHugo <36848957+LegrandHugo@users.noreply.github.com>
2018-02-27 09:37:45 +0100

    Create Fenetre.java

---
+++ Fenetre.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Fenetre extends JFrame
+{
+	public Fenetre() 
+	{
+		this.setTitle("Outil de Visualisation : TEST");
+		this.setSize(1200,800);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+}
