e91fb51a61e8638aa502eaa112cad695da1ff1e0
LegrandHugo <36848957+LegrandHugo@users.noreply.github.com>
2018-02-27 11:21:50 +0100

    Update Fenetre.java

--- Fenetre.java
+++ Fenetre.java
@@ -1,14 +1,17 @@
 import javax.swing.JFrame;
 
 public class Fenetre extends JFrame
-{
+{	
 	public Fenetre() 
 	{
 		this.setTitle("Outil de Visualisation : TEST");
 		this.setSize(1200,800);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Dessin pan = new Dessin();
+		
+		this.setContentPane(pan);
 		this.setVisible(true);
 	}
-
 }
